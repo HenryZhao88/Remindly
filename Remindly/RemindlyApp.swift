5df21c47fcdb4883ca0e152a67ea13bce08df624
henry mac <henryozhao@gmail.com>
2026-04-09 18:43:43 -0400

    chore: initial project setup with XcodeGen

---
+++ Remindly/RemindlyApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct RemindlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RemindlyTests/RemindlyTests.swift
@@ -0,0 +1,4 @@
+import XCTest
+@testable import Remindly
+
+final class RemindlyTests: XCTestCase {}
